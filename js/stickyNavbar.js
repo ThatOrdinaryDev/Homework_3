c512fffbc7285a04167316d9007838ccb55f06cc
ThatOrdinaryDev <sussysu1001@gmail.com>
2026-03-06 21:10:25 -0500

    idk

--- js/stickyNavbar.js
+++ js/stickyNavbar.js
@@ -1,14 +1,16 @@
+const navbar = document.getElementById("navbar");
 
-const navbar = document.getElementById('navbar');
+window.addEventListener("scroll", () => {
 
-document.addEventListener('scroll', () => {
-    let currentScroll = window.scrollY;
+  let currentScroll = window.scrollY;
 
-    if (currentScroll > 50) {
-        navbar.classList.add("nav-glass");
-        navbar.classList.add("nav-small");
-    } else {
-        navbar.classList.add("nav-glass");
-        navbar.classList.add("nav-small");
-    }
+  /* GLASS + SHRINK */
+  if(currentScroll > 50){
+    navbar.classList.add("nav-glass");
+    navbar.classList.add("nav-small");
+  }else{
+    navbar.classList.remove("nav-glass");
+    navbar.classList.remove("nav-small");
+  }
+
 });
